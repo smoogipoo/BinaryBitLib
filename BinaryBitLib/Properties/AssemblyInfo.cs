3cca29b9c62c209233c0b3ba03bcd14d4c7b8eac
smoogipooo <smoogipooo@gmail.com>
2015-08-06 19:53:35 +0930

    Update some info, upload to nuget!

--- BinaryBitLib/Properties/AssemblyInfo.cs
+++ BinaryBitLib/Properties/AssemblyInfo.cs
@@ -6,13 +6,13 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BinaryBitLib")]
-[assembly: AssemblyDescription("A C# library to operate on data bit-by-bit.")]
+[assembly: AssemblyDescription("An open source .NET library to operate on data bit-by-bit.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("smoogipooo")]
 [assembly: AssemblyProduct("BinaryBitLib")]
 [assembly: AssemblyCopyright("Copyright © smoogipooo 2015")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("en-US")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
